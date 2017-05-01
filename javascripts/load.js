123343834e1464c615f0ebfae7bf7ef3da6f4c4e
Madeline Power <madelinepower@Madelines-MacBook-Pro.local>
2017-05-01 14:02:48 -0500

    load.js and fixed json files bullshitttttttttt lalala

---
+++ javascripts/load.js
@@ -0,0 +1,19 @@
+var Chatty = (function(originalChatty) {
+
+  originalChatty.loadJSON = function() {
+      var dataRequest = new XMLHttpRequest();
+      dataRequest.addEventListener("load", dataRequestLoadComplete);
+      dataRequest.addEventListener("error", dataRequestError);
+
+      function dataRequestLoadComplete(event){
+        console.log("Pickup line data has LOADED");
+        var pickupLines = JSON.parse(event.target.responseText);
+        console.log("pickup lines", pickupLines);
+      }
+      function dataRequestError(event){
+        console.log("Pickup line data has error");
+      }
+      dataRequest.open("GET", "messages.json");
+      dataRequest.send();
+  }
+});
